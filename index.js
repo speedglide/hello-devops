cc7b7913703c9db5bf445a8e0cdd5fb3c8a9a07c
speedglide <speedglide@comcast.net>
2020-06-17 21:20:59 -0400

    Change from JSON to HTML

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const process = require('process');
 
 var expressapp = express();
 expressapp.get('/', function (req, res) {
-    res.send('{"message":"Hello Devops as JSON from a nodejs server REST API v1"}');
+    // res.send('{"message":"Hello Devops as JSON from a nodejs server REST API v1"}');
+    res.send('<!DOCTYPE html><html lang="en"><head><meta charset="utf-8"><title>Hello-DevOps</title></head><body><H1>Hello DevOps from Node JS</H1></body></html>');    
 })
 expressapp.listen(80, function () {
     console.log('Ready on port 80');
